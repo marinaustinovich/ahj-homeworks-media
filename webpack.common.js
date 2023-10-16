8857fd1e4bad038e32b80e10c9465047d25ae5c6
marinaustinovich <115401561+marinaustinovich@users.noreply.github.com>
2023-10-16 14:49:38 +0300

    fix linting errors

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,14 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // eslint-disable-line import/no-extraneous-dependencies
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   target: 'web',
   devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'assets/[hash][ext][query]', // Все ассеты будут
-    // складываться в dist/assets
+    assetModuleFilename: 'assets/[hash][ext][query]',
     clean: true,
   },
   entry: './src/index.js',
@@ -20,36 +19,33 @@ module.exports = {
         type: 'asset',
         parser: {
           dataUrlCondition: {
-            maxSize: 4 * 1024, // 4kb
+            maxSize: 4 * 1024,
           },
         },
       },
       {
         test: /\.js$/,
-        exclude: /node_modules/, // не обрабатываем файлы из node_modules
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            cacheDirectory: true, // Использование кэша для избежания рекомпиляции
-            // при каждом запуске
+            cacheDirectory: true,
           },
         },
       },
-      { test: /\.(html)$/, use: ['html-loader'] }, // Добавляем загрузчик для html
+      { test: /\.(html)$/, use: ['html-loader'] },
       {
-        test: /\.(s[ac]|c)ss$/i, // /\.(le|c)ss$/i если вы используете less
+        test: /\.(s[ac]|c)ss$/i,
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
           'postcss-loader',
           'sass-loader',
         ],
-      }, // Добавляем загрузчики стилей
+      },
       {
         test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
-        type: 'asset/resource', // В продакшен режиме
-        // изображения размером до 8кб будут инлайнится в код
-        // В режиме разработки все изображения будут помещаться в dist/assets
+        type: 'asset/resource',
       },
       {
         test: /\.(woff2?|eot|ttf|otf)$/i,
@@ -59,10 +55,10 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './src/index.html', // Данный html будет использован как шаблон
+      template: './src/index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css', // Формат имени файла
+      filename: '[name].[contenthash].css',
     }),
     new CleanWebpackPlugin(),
   ],
